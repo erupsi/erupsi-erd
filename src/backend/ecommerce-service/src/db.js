170aee7d3d15c22fa9eff403e9133ef5c5249e7e
ZAZW27 <clawsrage27@gmail.com>
2025-07-27 18:42:35 +0800

    Fix: Fixed on node_modules and other ignored gti to be certainly ignored in git

---
+++ src/backend/ecommerce-service/src/db.js
@@ -0,0 +1,11 @@
+const {Pool} = require("pg");
+
+const pool = new Pool({
+    user: "ecomuser",
+    host: "localhost",
+    database: "ecomdb",
+    password: "ecompass",
+    port: 5436,
+});
+
+module.exports = pool;
